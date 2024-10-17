d9d65e7dca767bc0c6db8cd3ad215f29f6c22cc2
Lizard Wizard <tung09102002@gmail.com>
2024-10-16 20:27:00 -0600

    add imgui

--- xtr_main.cpp
+++ xtr_main.cpp
@@ -1,5 +1,8 @@
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl2.h"
 #include <SDL2/SDL.h>
 #include <glad/gl.h>
+
 int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
@@ -13,8 +16,17 @@ int main(int argc, char *argv[]) {
         SDL_CreateWindow("XToon Renderer", SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
 
-    SDL_GLContext context = SDL_GL_CreateContext(window);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
     glViewport(0, 0, 800, 600);
 
@@ -22,19 +34,34 @@ int main(int argc, char *argv[]) {
     while (xtr_running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
             if (event.type == SDL_QUIT) {
                 xtr_running = false;
             }
         }
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+        ImGui::Begin("panel");
+        ImGui::End();
+
+        ImGui::Render();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         SDL_GL_SwapWindow(window);
         SDL_Delay(1);
     }
-    SDL_GL_DeleteContext(context);
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
